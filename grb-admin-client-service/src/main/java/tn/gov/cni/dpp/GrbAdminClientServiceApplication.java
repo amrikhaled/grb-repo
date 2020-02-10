9e47b73b7d7fdce3768e122cf1ecc7690e46b070
Your Name <kamri@cni.tn>
2020-02-10 15:58:28 +0100

    create projects
    Signed-off-by: Your Name <kamri@cni.tn>

---
+++ grb-admin-client-service/src/main/java/tn/gov/cni/dpp/GrbAdminClientServiceApplication.java
@@ -0,0 +1,15 @@
+package tn.gov.cni.dpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GrbAdminClientServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GrbAdminClientServiceApplication.class, args);
+	}
+
+}
