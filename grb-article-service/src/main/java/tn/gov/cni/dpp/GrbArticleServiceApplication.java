f72457b6dd823116b70c1128547846135d553030
Your Name <kamri@cni.tn>
2020-02-12 16:17:58 +0100

    registry service
    Signed-off-by: Your Name <kamri@cni.tn>

--- grb-article-service/src/main/java/tn/gov/cni/dpp/GrbArticleServiceApplication.java
+++ grb-article-service/src/main/java/tn/gov/cni/dpp/GrbArticleServiceApplication.java
@@ -4,12 +4,14 @@ import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 import tn.gov.cni.dpp.domain.Bien;
 import tn.gov.cni.dpp.repository.BienRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GrbArticleServiceApplication {
 
 	public static void main(String[] args) {
@@ -17,8 +19,6 @@ public class GrbArticleServiceApplication {
 		BienRepository bienRepository = applicationContext.getBean(BienRepository.class);
 
 		Stream.of("b1", "b2", "b3").forEach(b -> bienRepository.save(new Bien(b)));
-		
-		
 
 	}
 
