495595fa78afdde639de72341a5ad17b0422bd83
Your Name <kamri@cni.tn>
2020-02-12 15:50:27 +0100

    ribbon service
    Signed-off-by: Your Name <kamri@cni.tn>

---
+++ grb-ribbon-client-service/src/main/java/tn/gov/cni/dpp/GrbRibbonClientServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.cni.dpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrbRibbonClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrbRibbonClientServiceApplication.class, args);
+	}
+
+}
---
+++ grb-ribbon-service/src/main/java/tn/gov/cni/dpp/GrbRibbonServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.cni.dpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrbRibbonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrbRibbonServiceApplication.class, args);
+	}
+
+}
